e4ca6cf18583e49341fd5af3166d3b1da15a7cbb
victorpawlek <pawlek.v03@htlwienwest.at>
2021-12-12 14:15:25 +0100

    aufgabe 4

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,3 +1,4 @@
+
 if (workbox) {
   console.log(`Workbox is loaded`);
   workbox.setConfig({ debug: true });
@@ -8,8 +9,20 @@ if (workbox) {
       cacheName: 'roberts-cache',
     }),
   );
+  workbox.routing.registerRoute(
+  new RegExp('/images/.*.jpg'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'roberts-image-cache',
+  }),
+);
 } else {
   console.log(`Workbox didn't load`);
 }
-
 self.skipWaiting();
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body.message,
+    icon: 'img/icons/employees_192x192.png',
+  });
+});
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app" class="container d-flex flex-column justify-content-center align-items-center mt-5">
+    <button class="btn btn-success" @click="subscribe">Subscribe</button>
     <div v-if="showAlert" class="alert alert-danger" role="alert">
       Update vorhanden! Bitte App neu starten!
     </div>
     <h3>Hier könnte Ihre Werbung sein!</h3>
     <img src="/employees.jpg" width="50%" alt="menschen" /><br />
-    <div v-if="offline" class="alert alert-danger" role="alert">
-      You are offline!
-    </div>
+    <div v-if="offline" class="alert alert-danger" role="alert">You are offline!</div>
     <ButtonGet @get="fetchData"></ButtonGet>
     <CardView :employees="employees" @del="delEmployee"></CardView>
   </div>
@@ -48,10 +47,33 @@ export default {
         window.location.reload();
       }
     },
+    async subscribe() {
+      if (!('serviceWorker' in navigator)) {
+        console.log('no service worker!');
+        return;
+      }
+      const publicVapidKey="BCxAXKg2tvsTg0nN3BmAU4H8nuUT-GTBeY5Q1jGC7ED2z-7z5MHlKb5PsOhNwH7OC7yD40kO51t6Y9ZXf-QOpBU";
+      const registration = await navigator.serviceWorker.ready;
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: this.urlBase64ToUint8Array(publicVapidKey),
+      });
+      await axios.post('/subscribe', subscription);
+    },
+    urlBase64ToUint8Array(base64String) {
+      const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+      const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    },
   },
   created() {
     document.addEventListener('swUpdated', this.updateAvailable, { once: true });
-    this.offline=!navigator.online
+    this.offline = !navigator.online;
     window.addEventListener('online', () => (this.offline = false));
     window.addEventListener('offline', () => (this.offline = true));
   },
